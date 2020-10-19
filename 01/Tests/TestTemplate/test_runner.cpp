1ccdc15c1f65beaa51c10130956675837f4cf177
Anton Labutin <Anton.Labutin.1997@yandex.ru>
2020-10-19 23:24:32 +0300

    update TestTemplate

--- 01/Tests/TestTemplate/test_runner.cpp
+++ 01/Tests/TestTemplate/test_runner.cpp
@@ -10,6 +10,12 @@
 
 using namespace std;
 
+void
+Assert(bool b, const string& hint)
+{
+    AssertEqual(b, true, hint);
+}
+
 TestRunner::~TestRunner()
 {
     if (fail_count > 0) {
--- 01/Tests/TestTemplate/test_runner.hpp
+++ 01/Tests/TestTemplate/test_runner.hpp
@@ -15,7 +15,6 @@
 #include <string>
 
 
-
 class TestRunner {
 public:
     template <class TestFunc>
